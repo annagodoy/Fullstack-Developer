aab6df01099abd813a4a68ad928c7085d2a185d9
Your Name <annabfgodoy@gmail.com>
2026-09-09 01:10:39 -0300

    feat: restrict admin access by role

---
+++ test/controllers/admin/dashboard_controller_test.rb
@@ -0,0 +1,32 @@
+require "test_helper"
+
+class Admin::DashboardControllerTest < ActionDispatch::IntegrationTest
+    test "redirect visitors to login page" do
+        get admin_root_path
+
+        assert_redirected_to new_session_path
+    end
+
+    test "deny access to members" do
+        user = users(:one)
+
+        assert user.member?
+        sign_in_as(user)
+
+        get admin_root_path
+
+        assert_response :forbidden
+    end
+
+    test "allow access to admins" do
+        user = users(:one)
+
+        user.update!(role: :admin)
+        sign_in_as(user)
+
+        get admin_root_path
+
+        assert_response :success
+        assert_select "h1", "Admin Dashboard"
+    end
+end
